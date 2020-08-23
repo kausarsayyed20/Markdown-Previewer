aab526d9cff6226f23c1b97e1378b8364e77edf6
kausarsayyed20 <kausarsayyed.20@gmail.com>
2020-08-23 21:35:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+//import marked from 'marked';
+let marked=require("marked");
+class App extends Component
+{
+  state={
+    text:''
+  }
+  handleChange=(e)=>{
+    this.setState({
+      text: e.target.value
+    })
+  }
+  render(){
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const {text}= this.state;
+    const markdown=marked(text);
+    return(
+      <div id="wrapper"> 
+        <h2 className= "text-center m-4" >Convert Your Markdown</h2>
+        
+        <div className="row">
+           <div className="col-6">
+             <h6 className="text-center">EDITOR</h6>
+            <textarea  className="form-control" id="editor" value={text} onChange={this.handleChange}/>
+
+           </div>
+           <div className="col-6 " id="preview">
+           <h6 className="text-center">PREVIEWER</h6>
+           <div className="preview rounded" dangerouslySetInnerHTML={{__html:markdown}}>
+           
+            </div>
+            
+
+           </div>
+
+      </div>
     </div>
-  );
+
+
+    );
+  }
 }
+
+
 
 export default App;
